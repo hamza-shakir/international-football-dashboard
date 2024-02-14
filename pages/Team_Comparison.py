4953e936770c1d37a6acfbf97caec196ddc97404
Hamza <hamzashakir149@gmail.com>
2024-02-14 18:57:51 +0530

    created additonal page more visual analysis

---
+++ pages/Team_Comparison.py
@@ -0,0 +1,5 @@
+import streamlit as st
+
+st.markdown("# Team Comparison Page ❄️")
+st.sidebar.markdown("Compare teams across the globe and see how they have performed over the years in various tournaments")
+st.write("Under developemnt")
