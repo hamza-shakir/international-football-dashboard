bd3c3666209c2b4ea17d82445277b46c7614feb1
Hamza <hamzashakir149@gmail.com>
2024-02-16 15:25:34 +0530

    deleting page selection from sidebar and merged it into one page

--- pages/Team_Comparison.py
+++
@@ -1,5 +0,0 @@
-import streamlit as st
-
-st.markdown("# Team Comparison Page ❄️")
-st.sidebar.markdown("Compare teams across the globe and see how they have performed over the years in various tournaments")
-st.write("Under developemnt")
